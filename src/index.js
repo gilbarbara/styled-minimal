5d331fa6adbabb226c7f03208cf9479f1bfc95c4
Gil Barbara <gilbarbara@gmail.com>
2018-08-03 00:45:13 -0300

    basic components

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { default as Alert } from './ui/Alert';
+export { default as Button } from './ui/Button';
+export { default as Container } from './ui/Container';
+export { default as Screen } from './ui/Screen';
+export { default as Typography } from './ui/Typography';
