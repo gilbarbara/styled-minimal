36958d93f4efd786b4ece160ad81ffc429862d84
Gil Barbara <gilbarbara@gmail.com>
2018-08-12 18:38:26 -0300

    ✨ Add Table

---
+++ src/Table.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import { fontFamily, TableStyles } from './utils';
+
+export const StyledTable = styled.table`
+  ${TableStyles.backgroundColor};
+  ${TableStyles.border};
+  ${TableStyles.color};
+  border-collapse: collapse;
+  ${fontFamily};
+  width: 100%;
+  
+  th {
+    text-align: inherit;
+  }
+
+  th,
+  td {
+    ${TableStyles.cellBorder};
+    ${TableStyles.padding};
+    vertical-align: top;
+  }
+  
+  thead {
+    ${TableStyles.headBackgroundColor};
+    ${TableStyles.headColor};
+  }
+  
+  thead th {
+    ${TableStyles.headCellBorder};
+    vertical-align: bottom;
+  }
+  
+  tbody tr:nth-of-type(odd) {
+    ${TableStyles.striped};
+  }
+  
+  ${TableStyles.caption};
+`;
+
+const Table = ({ children, ...props }) => (
+  <StyledTable {...props}>{children}</StyledTable>
+);
+
+Table.propTypes = {
+  bordered: PropTypes.bool,
+  borderless: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+  head: PropTypes.oneOf(['light', 'dark']),
+  inverted: PropTypes.bool,
+  size: PropTypes.oneOf(['sm', 'md', 'lg']),
+  striped: PropTypes.bool,
+};
+
+Table.defaultProps = {
+  bordered: false,
+  borderless: false,
+  inverted: false,
+  size: 'md',
+  striped: false,
+};
+
+export default Table;
---
+++ stories/Table.stories.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs, boolean, select } from '@storybook/addon-knobs/react';
+
+import { Table } from '../src';
+import { View } from './helpers/components';
+import { backgroundAddon } from './helpers/extras';
+
+storiesOf('Table', module)
+  .addDecorator(backgroundAddon)
+  .addDecorator(withKnobs)
+  .add('default', withInfo({
+    propTablesExclude: [View],
+    text: 'Table wrapper',
+  })(() => (
+    <View skipCentered>
+      <Table
+        bordered={boolean('Bordered', false)}
+        borderless={boolean('Borderless', false)}
+        size={select('Size', ['sm', 'md', 'lg'], 'md')}
+        head={select('Head Color', ['', 'light', 'dark'])}
+        inverted={boolean('Inverted', false)}
+        striped={boolean('Striped', false)}
+      >
+        <caption>List of users</caption>
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </View>
+  )));
