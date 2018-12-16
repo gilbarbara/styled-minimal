94e8ecf3d62ad360bd617503b70136819f81cf26
Gil Barbara <gilbarbara@gmail.com>
2018-12-16 05:36:25 -0200

    🔅 Add flex properties to Box directly instead of Flex

--- src/Flex.jsx
+++ src/Flex.jsx
@@ -1,42 +1,22 @@
-import React from 'react'; //eslint-disable-line no-unused-vars
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import {
-  alignContent,
-  alignItems,
-  flexBasis,
-  flexDirection,
-  flexWrap,
-  justifyContent,
-} from 'styled-system';
-
 import Box from './Box';
 
-export const Flex = styled(Box)(
-  {
-    display: 'flex',
-  },
-  alignContent,
-  alignItems,
-  flexBasis,
-  flexDirection,
-  flexWrap,
-  justifyContent,
-);
+export const Flex = styled(Box)({
+  display: 'flex',
+});
 
 Flex.displayName = 'Flex';
 
 Flex.propTypes = {
   as: PropTypes.string,
   children: PropTypes.node.isRequired,
-  ...alignContent.propTypes,
-  ...alignItems.propTypes,
-  ...flexBasis.propTypes,
-  ...flexDirection.propTypes,
-  ...flexWrap.propTypes,
-  ...justifyContent.propTypes,
   ...Box.propTypes,
+};
+
+Flex.defaultProps = {
+  alignItems: 'center',
 };
 
 export default Flex;
