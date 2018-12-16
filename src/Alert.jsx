858024960893ca7394720b06b1c06b60c0905f54
Gil Barbara <gilbarbara@gmail.com>
2018-12-16 05:37:09 -0200

    🔥 Remove unused React import

--- src/Alert.jsx
+++ src/Alert.jsx
@@ -1,4 +1,3 @@
-import React from 'react'; //eslint-disable-line no-unused-vars
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 
--- src/Code.jsx
+++ src/Code.jsx
@@ -1,4 +1,3 @@
-import React from 'react'; //eslint-disable-line no-unused-vars
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
